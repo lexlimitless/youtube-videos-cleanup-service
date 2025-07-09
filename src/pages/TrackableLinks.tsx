8c693a84e86b656904597009888cb82fb1f1534c
lexlimitless <paul@amazinggains.net>
2025-07-08 22:30:06 -0500

    chore: update default short link domain to www.pepperlytics.com

--- src/pages/TrackableLinks.tsx
+++ src/pages/TrackableLinks.tsx
@@ -9,7 +9,7 @@ import YouTubeVideoGrid from '../components/YouTubeVideoGrid';
 
 const platforms = ['YouTube', 'Instagram'];
 const attributionWindows = [1, 7, 14];
-const SHORT_LINK_DOMAIN = import.meta.env.VITE_SHORT_LINK_DOMAIN || 'https://pepperlytics.com';
+const SHORT_LINK_DOMAIN = import.meta.env.VITE_SHORT_LINK_DOMAIN || 'https://www.pepperlytics.com';
 
 interface YouTubeVideo {
   id: string;
